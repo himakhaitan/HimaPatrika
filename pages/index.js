327d64ce80eb8108d1326d9d8912be610da66e65
Himanshu Khaitan <himanshukhaitan108@gmail.com>
2022-01-25 02:13:55 +0530

    Meta tags added, Navigation Laid

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import classes from "../styles/Home.module.scss";
-
-export default function Home() {
-  return (
-    <div className={classes.main}>
-      <h1>Hello World</h1>
-    </div>
-  );
-}
