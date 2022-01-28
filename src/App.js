1867d644a1e32652be513f0e5a7ddec1e6b1d1dc
Himanshu Khaitan <himanshukhaitan108@gmail.com>
2022-01-28 15:45:46 +0530

    Details Added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,24 @@ function App() {
         quote="Investing in Yourself is not about you. It's about putting your stamp on the value you deliver to others."
         author="William Arruda"
       />
+      <div className="owner">
+        <div className="owner__img--div">
+          <img className="owner--img" src="./images/me.jpg" />
+        </div>
+        <div className="ownerDetails">
+          <h4>Namaste, This is</h4>
+          <h2>Himanshu Khaitan.</h2>
+          <h2>adding Values while acquiring skills</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc auctor
+            mauris eu neque sagittis, lacinia mattis ante vehicula. Vestibulum
+            quis convallis elit. Aenean sit amet erat urna. In maximus quam
+            mauris. Quisque lobortis maximus nulla nec suscipit. Duis et dictum
+            lacus, at sagittis justo.
+          </p>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
